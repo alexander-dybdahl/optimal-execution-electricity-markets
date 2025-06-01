2b3e3919df079118ab109a7fc8c1eae07aa4a09b
alexander-dybdahl <alexldybdahl@gmail.com>
2025-06-01 16:32:24 +0200

    Add analytical_known attribute to FBSNN class for configuration flexibility

--- core/fbsnn.py
+++ core/fbsnn.py
@@ -57,6 +57,7 @@ class FBSNN(nn.Module, ABC):
         self.dim = model_cfg["dim"]        # state space dimension
         self.dim_W = model_cfg["dim_W"]    # Brownian motion dimension
         self.y0 = torch.tensor([model_cfg["y0"]], device=self.device, requires_grad=True)
+        self.analytical_known = args.analytical_known
 
         # Saving & Checkpointing
         self.save = args.save              # e.g., "best", "every", "last"
